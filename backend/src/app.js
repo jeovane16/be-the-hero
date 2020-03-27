5bb81722e5823d003951e4cb8ab1f84e3f3d6b26
jeovane16 <jeovanearaujo16@gmail.com>
2020-03-27 14:35:09 -0300

    Teste no back-end da aplicação

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const routes = require('./routes');
 const cors = require('cors');
+const { errors } = require('celebrate');
 
 const express = require('express');
 
@@ -8,5 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
